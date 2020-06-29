f31a8244cc379366f3be12f7794a72839044e884
guptaprakhariitr <prakharpguptag@gmail.com>
2020-06-29 20:13:20 +0530

    add

--- octo/.venv/bin/django-admin.py
+++
@@ -1,7 +0,0 @@
-#!/home/prakhar/Projects/Django/octo-backend/octo/.venv/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==3.1','django-admin.py'
-__requires__ = 'Django==3.1'
-__import__('pkg_resources').require('Django==3.1')
-__file__ = '/home/prakhar/django/django/bin/django-admin.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
