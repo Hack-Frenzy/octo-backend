c7d99da201a0fb3e75c46a4727cbf851d94cca07
Akanksha <akankshaverma282007@gmail.com>
2020-06-30 18:37:26 +0530

    login page

--- octo/octo/settings.py
+++ octo/octo/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
 STATIC_URL = '/static/'
+
 LOGIN_REDIRECT_URL='allPatients'
 LOGIN_URL='login'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
