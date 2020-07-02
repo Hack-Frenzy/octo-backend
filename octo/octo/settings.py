2f4e6e54fa52cc493c0a7596850f4d52ddbe527a
guptaprakhariitr <prakharpguptag@gmail.com>
2020-07-03 00:03:22 +0530

    reuire.txt

--- octo/octo/settings.py
+++ octo/octo/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL='allPatients'
-LOGIN_URL='login'
+LOGIN_REDIRECT_URL = 'allPatients'
+LOGIN_URL = 'login'
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
