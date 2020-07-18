728123740fdec6c730dd71df8273f8a38614c6d5
Ishan-002 <ishan26rawat@gmail.com>
2020-07-19 00:13:35 +0530

    Added .env file for secret credentials

--- octo/pyrebase_settings.py
+++ octo/pyrebase_settings.py
@@ -1,11 +1,12 @@
 import pyrebase
+from decouple import config
 
 config = {
-    "apiKey": "<censored>",
-    "authDomain": "<censored>",
-    "databaseURL": "<censored>",
-    "storageBucket": "<censored>",
-    "serviceAccount": "<censored>"
+    "apiKey": config('API_KEY'),
+    "authDomain": config('AUTH_DOMAIN'),
+    "databaseURL": config('DATABASE_URL'),
+    "storageBucket": config('STORAGE_BUCKET'),
+    "serviceAccount": config('SERVICE_ACCOUNT'),
 }
 
 firebase = pyrebase.initialize_app(config)
